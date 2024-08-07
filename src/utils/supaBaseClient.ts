8e6d3eb5ef6d197995c96e0be880738058c595c7
benbourgon <benjaminbourgon@gmail.com>
2024-08-07 17:09:53 -0400

    style header, to-do list, form

---
+++ src/utils/supaBaseClient.ts
@@ -0,0 +1,9 @@
+// utils/supaBaseClient.ts
+
+// Supabase client
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
